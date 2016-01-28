c1ebf5e81ba014b50bb88a8ac755c261e7e6f2f3
freejoe76 <jmurphy@denverpost.com>
2016-01-28 11:17:04 -0500

    We need more filesize here, cap'n

--- www/index.php
+++ www/index.php
@@ -1,7 +1,7 @@
 <?php
 ini_set('display_startup_errors',1);
 ini_set('display_errors',1);
-ini_set('upload_max_filesize', '3000M');
+ini_set('upload_max_filesize', '6000M');
 ini_set('post_max_size', '3000M');
 ini_set('max_input_time', 30000);
 ini_set('max_execution_time', 30000);
